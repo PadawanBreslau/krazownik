9aa6c4cce46b67c46b0a26ea835cfbebe5a08689
Staszek <s.zawadzki@visuality.pl>
2020-05-19 10:30:00 +0200

    Notes refactoring

--- app/components/HomePageView/index.js
+++ app/components/HomePageView/index.js
@@ -10,18 +10,19 @@ export default class HomePageView extends React.PureComponent {
       <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.info}>
-            <h1>Krążownik</h1>
-            <h3>Szczawnica, 6-7.06.2020</h3>
+            <h1>Krążownik 2020 | Szczawnica, 6-7.06.2020</h1>
+            <div className={styles.notes}>
             {notes && notes.map((note) => (
-              <>
+              <div className={styles.note}>
                 <span className={styles.title}>
                   {note.title} { moment(note.createdAt).locale('pl').format('l')}
                 </span>
                 <span className={styles.alert}>
                   {note.content}
                 </span>
-              </>
+              </div>
             ))}
+            </div>
           </div>
         </div>
       </div>
