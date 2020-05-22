1e3d00aceadd0169438f8e8ee7a55a82800e4f71
Staszek <s.zawadzki@visuality.pl>
2020-05-22 10:38:41 +0200

    Redirect to ssl

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const { resolve } = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
+app.use(sslRedirect());
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
