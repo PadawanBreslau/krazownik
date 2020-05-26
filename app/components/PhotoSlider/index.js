1570cf9a3d1f1b3711a12f4471e3c764aef9a58d
Staszek <s.zawadzki@visuality.pl>
2020-05-26 19:24:07 +0200

    Photo slider

---
+++ app/components/PhotoSlider/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import config from '../../config';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+
+const PhotoSlider = ({ photos }) => {
+  const backendUrl = config.api.url;
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
+  const slider = (
+    <AutoplaySlider
+      interval={6000}
+      play={true}
+      cancelOnInteraction={false} // should stop playing on user interaction
+      animation="cubeAnimation">
+      {photos && photos.map((photo) => (
+        <div><img src={backendUrl + photo.url} /></div>
+      ))}
+    </AutoplaySlider>
+  );
+  return (
+    <>{slider}</>
+  )
+}
+
+PhotoSlider.propTypes = {
+  photos: PropTypes.array,
+}
+
+export default PhotoSlider
+
