f341ca65152345cd579e241657120929ce1a32ee
Staszek <s.zawadzki@visuality.pl>
2021-04-03 07:51:00 +0200

    Material UI icons

--- app/components/TeamView/TeamTask.js
+++ app/components/TeamView/TeamTask.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PhotoFrame from './PhotoFrame';
+import { ChevronRight, ChevronLeft } from '@material-ui/icons';
 import styles from './styles.scss';
 
 const TeamTask = ({ task, teamId }) => {
   const [photosVisible, setPhotosVisible] = useState(false);
   const photosLeft = task.amount - (task.teamTaskPhotos?.length || 0);
+  const chevron = photosVisible ? <ChevronLeft/> : <ChevronRight/>
 
   return (
     <div className={styles.teamTask}>
       <div className={styles.rowWithContent}>
-        <div className={styles.taskContent} onClick={() => setPhotosVisible(!photosVisible)}>{task.content}</div>
+        <div className={styles.taskContent} onClick={() => setPhotosVisible(!photosVisible)}>{task.content} {chevron}</div>
         {photosVisible && (
           <div className={styles.photoContent}>
             {task.teamTaskPhotos &&
