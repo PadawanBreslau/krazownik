534bcdd0f41768950f0f5ddcd8b8487703943154
Staszek <s.zawadzki@visuality.pl>
2021-04-03 07:33:50 +0200

    Expandable tasks

--- app/components/TeamView/TeamTask.js
+++ app/components/TeamView/TeamTask.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PhotoFrame from './PhotoFrame';
 import styles from './styles.scss';
 
 const TeamTask = ({ task, teamId }) => {
+  const [photosVisible, setPhotosVisible] = useState(false);
   const photosLeft = task.amount - (task.teamTaskPhotos?.length || 0);
 
   return (
     <div className={styles.teamTask}>
       <div className={styles.rowWithContent}>
-        <div className={styles.taskContent}>{task.content}</div>
-        <div className={styles.photoContent}>
-          {task.teamTaskPhotos && task.teamTaskPhotos.map((photo) => <PhotoFrame photo={photo} />)}
+        <div className={styles.taskContent} onClick={() => setPhotosVisible(!photosVisible)}>{task.content}</div>
+        {photosVisible && (
+          <div className={styles.photoContent}>
+            {task.teamTaskPhotos &&
+              task.teamTaskPhotos.map((photo) => <PhotoFrame photo={photo} />)}
 
-          {photosLeft > 0 &&
-            [...Array(photosLeft)].map(() => (
-              <PhotoFrame taskId={task.id} teamId={teamId}/>
-            ))}
-        </div>
+            {photosLeft > 0 &&
+              [...Array(photosLeft)].map(() => <PhotoFrame taskId={task.id} teamId={teamId} />)}
+          </div>
+        )}
       </div>
     </div>
   );
